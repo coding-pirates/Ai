78135d9e2ffc7f235a13572a0b33c8aa284ac03e
paube <paube@mail.upb.de>
2019-11-27 21:36:32 +0100

    ai start

---
+++ src/main/java/de/upb/codingpirates/battleships/ai/Hnadler.java
@@ -0,0 +1,11 @@
+package de.upb.codingpirates.battleships.ai;
+
+import de.upb.codingpirates.battleships.client.Handler;
+import de.upb.codingpirates.battleships.network.message.notification.GameInitNotification;
+
+public class Hnadler implements Handler {
+    @Override
+    public void handleGameInit(GameInitNotification message) {
+
+    }
+}
