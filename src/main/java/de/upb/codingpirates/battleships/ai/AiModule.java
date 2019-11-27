91a3e2a140c68086dbd4bbd556cd2a714101b12c
paube <paube@mail.upb.de>
2019-11-27 22:17:34 +0100

    move class

--- src/main/java/de/upb/codingpirates/battleships/ai/AiModule.java
+++ src/main/java/de/upb/codingpirates/battleships/ai/AiModule.java
@@ -1,7 +1,7 @@
 package de.upb.codingpirates.battleships.ai;
 
-import de.upb.codingpirates.battleships.client.AbstractClientModule;
 import de.upb.codingpirates.battleships.client.Handler;
+import de.upb.codingpirates.battleships.client.network.AbstractClientModule;
 
 public class AiModule extends AbstractClientModule {
     @Override
