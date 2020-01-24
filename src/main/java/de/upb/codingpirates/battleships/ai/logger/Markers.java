2b532c16dd90e949e8a666e76351a1cd54795ca4
Andre Blanke <blanke.and@gmail.com>
2020-01-24 14:01:14 +0100

    Remove unused Markers

--- src/main/java/de/upb/codingpirates/battleships/ai/logger/Markers.java
+++ src/main/java/de/upb/codingpirates/battleships/ai/logger/Markers.java
@@ -7,15 +7,14 @@ import org.apache.logging.log4j.MarkerManager;
  * Implements the markers for log4J logs.
  */
 public class Markers {
-    public final static Marker Ai_Main = MarkerManager.getMarker("Ai_Main");
+
     public final static Marker Ai = MarkerManager.getMarker("Ai");
-    public final static Marker Ai_Heatmap = MarkerManager.getMarker("Ai_HeatmapCreator");
     public final static Marker Ai_ShotPlacer = MarkerManager.getMarker("Ai_ShotPlacer");
-    public final static Marker Ai_Hits = MarkerManager.getMarker("Ai_HitsHandler");
     public final static Marker Ai_Misses = MarkerManager.getMarker("Ai_MissesFinder");
     public final static Marker Ai_SunkenShips = MarkerManager.getMarker("Ai_SunkenShipsHandler");
     public final static Marker Ai_ShipPlacer = MarkerManager.getMarker("ShipPlacer");
 
+    /* Prevent instantiation. */
     private Markers() {
     }
 }
